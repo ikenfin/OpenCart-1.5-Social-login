7eb94c8dc9b7445b563a13c37e94bc7e5c241c8f
ikenfin <ikenfin@gmail.com>
2018-01-30 20:21:09 +0400

    Добавил отсутствующие переводы

---
+++ catalog/language/english/module/sociallogin.php
@@ -0,0 +1,7 @@
+<?php
+$_['text_subject']			= 'Registration in online store %s - Your account password';
+$_['text_help']				= '<p>Dear, %s %s, Welcome to online store %s!</p>
+								<p>You have been registered on the site <a href="%s">%s</a> with a social network.<br/>
+								For subsequent authorizations, you can use the input through social. Network or use your E-mail and password: \"%s\" (without the quotes).</p>
+								';
+?>
