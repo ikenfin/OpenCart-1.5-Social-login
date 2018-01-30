e657bf9a94d1ac6ac9e4c1c41cef356b06d03149
ikenfin <ikenfin@gmail.com>
2018-01-30 20:04:14 +0400

    :alien: Added english translation

---
+++ admin/language/english/module/sociallogin.php
@@ -0,0 +1,58 @@
+<?php
+// Heading
+$_['heading_title']			= '[ikenfin] Social network auth';
+
+// Tabs
+$_['tab_general']	    = 'Description';
+$_['tab_facebook']	    = 'FaceBook';
+$_['tab_vkontakte']   	= 'Vkontakte (vk.com)';
+$_['tab_twitter']   	= 'Twitter';
+$_['tab_instagram']     = 'Instagram';
+$_['tab_ok']           	= 'Odnoklassniki (ok.ru)';
+
+// Entry
+$_['entry_status']				= 'Status:';
+$_['entry_auth_url']            = 'Auth route';
+
+$_['entry_facebook']			= 'FaceBook';
+$_['entry_facebook_description'] = 'Facebook auth settings:';
+$_['entry_facebook_appid']		= 'App ID/API Key:';
+$_['entry_facebook_appsecret']	= 'App Secret:';
+
+$_['entry_twitter']		= 'Twitter';
+$_['entry_twitter_description'] = 'Twitter auth settings:';
+$_['entry_twitter_appid'] = 'App ID:';
+$_['entry_twitter_consumerkey'] = 'Consumer Key:';
+$_['entry_twitter_consumersecret'] = 'Consumer Secret:';
+
+$_['entry_vkontakte']			= 'Vkontakte (vk.com)';
+$_['entry_vkontakte_description'] = 'VK auth settings:';
+$_['entry_vkontakte_appid']		= 'App ID:';
+$_['entry_vkontakte_appsecret']	= 'Secret key:';
+
+$_['entry_ok']          = 'Odnoklassniki (ok.ru)';
+$_['entry_ok_description'] = 'Odnoklassniki auth settings:';
+$_['entry_ok_appid']    = 'App id';
+$_['entry_ok_appsecret']    = 'App secret';
+$_['entry_ok_apppublic']    = 'App public';
+
+$_['entry_instagram'] = 'Instagram';
+$_['entry_instagram_description'] = 'No integration at time. You can contribute to this project <a href="https://github.com">here</a>';
+
+$_['button_save_go']			= 'Save and close';
+$_['button_save_stay']			= 'Save';
+
+
+// Text
+$_['text_module']			= 'Modules';
+$_['text_success']			= 'Settings saved!';
+$_['text_description']		= '<p>This module based on early version of SocialLogin by Nikita_SP</p>
+								<p>Fixed <b>Facebook</b> auth, added <b>Twitter</b> и <b>Odnoklassniki</b> auth</p>
+								<p>Workflow: first-commer will authorize via social network and receive on email new account info.</p>
+
+								<p>Next time, user will auth to his account.</p>';
+$_['text_help']				= '<p>Module works over native social network API\'s, so you must create applications in each network by yourself.</p>';
+
+// Error
+$_['error_permission']		= 'You have no permission to control module!';
+?>
